d446e8807d9b27dd9b03d3963751d2d77b182f51
Ecommerce <calebestodulski@gmail.com>
2022-02-14 13:06:53 -0400

    created dbcontext class

---
+++ CRUD/Data/UserDbContext.cs
@@ -0,0 +1,13 @@
+using CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
